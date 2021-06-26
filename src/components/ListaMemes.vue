f54f26d93bc27822ad6add58dc2c431f5bab7f9e
Miguel Ramos <return.infox@gmail.com>
2021-06-25 21:44:43 -0400

    Aplicacion de Memex con Vuex Clase 6

---
+++ src/components/ListaMemes.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Listado de Memes</h1>
+  <select class="mt-5 form-select" v-model="cantidadMeme">
+    <option value=""> All </option>
+    <option value="5">5</option>
+    <option value="10">10</option>
+    <option value="20">20</option>
+  </select>
+
+  <div class="mt-5 col-10 offset-1 row">
+    <div class="card" style="width: 18rem;">
+      <img src="" class="card-img-top" alt="" />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    
+  </div>
+
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  data() {
+    return {
+      cantidadMeme: 0
+    }
+  },
+  computed: {
+    ...mapState(['memes'])
+  },
+  methods: {
+    ...mapActions(['obtenerMemes'])
+  }
+};
+</script>
+
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,44 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    title: "The Memes",
+    memes: []
   },
   mutations: {
+    /* Encargado de actualizar el State */
+    /* Payload son los nuevos datos */
+    actualizarMemes(state, payload) {
+      state.memes = payload;
+    }
+
   },
   actions: {
+    /* Podemos hacer llamdos a Apis */
+    async obtenerMemes({commit}, payload) {
+      try {
+        const data = await fetch('https://api.imgflip.com/get_memes');
+        const getMemes = await data.json();
+
+        if(!payload) {
+          console.log("Traemos todos los memes");
+          commit('actualizarMemes', getMemes);
+        } else {
+          const resultTemp = [];
+          // Aqui es donde le damos el tamaño al arreglo
+          getMemes.data.memes.ForEach((meme, index) => {
+            if( index < payload ) {
+              resultTemp.push(meme)
+            }
+          });
+          commit()
+        }
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
+
   modules: {
   }
 })
